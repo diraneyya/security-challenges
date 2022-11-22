5f33ef4eb4c1ea141e6d559ff99bfb945476d722
Orwa Diraneyya <diraneyya@wildcodeschool.com>
2022-11-22 12:23:34 +0100

    Updated answer

--- src/data.ts
+++ src/data.ts
@@ -81,7 +81,7 @@ const CHALLENGES_RAW: ChallengeEntry[] = [
   {
     name: "[XSS] Hack Search",
     url: "https://xss-game.appspot.com/level1",
-    solution: "Perseverance is the key",
+    solution: "Persistence is the key",
     level: 2,
     notice:
       "Injects an alert(). Once successful, enter the exact xss-game level 2 title to capture the flag",
